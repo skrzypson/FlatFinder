d428cb43518c2efa2343b64e38ccba49699f8455
skrzypson <krzys.skrzyp@gmail.com>
2023-08-11 22:53:46 +0200

    Update flat_finder.py

--- flat_finder.py
+++ flat_finder.py
@@ -1,17 +1,30 @@
-import requests
+import os
+import re
+
 import bs4
 import pandas as pd
-import os
-# import futures
+import requests
+from tqdm import tqdm
 
-class HouseMetaData():
+CLASSES = (
+    ('title', 'h1', 'css-bg3pmc er34gjf0'),
+    ('price', 'h2', 'css-5ufiv7 er34gjf0'),
+    ('description', 'div', 'css-bgzo2k er34gjf0'),
+)
 
-    url = \
-    """
-    https://www.olx.pl/nieruchomosci/mieszkania/krakow/?search%5Bfilter_float_price%3Afrom%5D=1500
-    &search%5Bfilter_float_price%3Ato%5D=2500&search%5Bfilter_float_m%3Afrom%5D=25&
-    search%5Bfilter_float_m%3Ato%5D=50&search%5Bprivate_business%5D=private
-    """
+
+def phrases_in_str(*phrases, target):
+    for p in phrases:
+        if re.search(p, target):
+            return True
+    return False
+
+
+
+class HouseMetaData:
+    base_url = "https://www.olx.pl/"
+    url = base_url + '/nieruchomosci/mieszkania/wynajem/krakow/'
+
 
     def __init__(self, url=None):
         self.page = None
@@ -19,97 +32,60 @@ class HouseMetaData():
         if url is None:
             self.url = HouseMetaData.url
         
+        self.offers_hrefs = []
         self.offers_df = pd.DataFrame(
-            {'title': [], 'price': [], 'link': [], 'contains_april': [], 'page': []}
-            ) 
+            {'title': [], 'price': [], 'description': [], 'contains-sept': [], 'contains-oct': []}
+        ) 
 
-    def next_page(self):
-        print(self.page)
-        if self.page == None:
-            self.page = 0
-        elif isinstance(self.page, int) and self.page < self.max_pages:
-            self.page += 1
-            self.url = self.url + f'&page={self.page+1}'
-    
-    def source_page(self):
-        page = requests.get(self.url)
+    def source_href(self, page=0):
+        page = requests.get(self.url + f'?page={page}')
         soup = bs4.BeautifulSoup(page.content, 'html.parser')
-        res = soup.find(id='offers_table')
-        self.offers = res.find_all('div', class_= 'offer-wrapper')
+        res = soup.find(**{'class': 'css-oukcj3'})
+        refs = res.find_all('a', class_= 'css-rc5s2u')
+        for item in refs:
+            self.offers_hrefs.append(item.attrs.get('href'))
+            
             
     def extract_all_offer_data(self):
-        print(f'Extracting homes from page {self.page}')
+        print(f'Extracting flats from page {self.page}')
 
-        for i in self.offers:
-            link = i.find('a', "marginright5 link linkWithHash detailsLinkPromoted", href=True)
-            if link is None:
-                link = i.find('a', "marginright5 link linkWithHash detailsLink", href=True)
-            price = i.find('p', class_="price")
-            record = pd.DataFrame(
-                {
-                    'title': [link.text.strip()],
-                    'price': [price.text.strip()], 
-                    'link': [link['href'].strip()], 
-                    'contains_april': ['-'], 
-                    'page': [self.page]
-                }
-            )
+        for link in tqdm(self.offers_hrefs):
+            page = requests.get(self.base_url + link)
+            soup = bs4.BeautifulSoup(page.content, 'html.parser')
+            
+            record = dict(link=(self.base_url + link))
+            broken = False
+            for thing, element, class_ in CLASSES:
+                content = soup.find(element, class_=class_)
+                if not content:
+                    broken = True
+                    break
+                record[thing] = [content.string]
+            
+            if broken:
+                continue
+            
+            for place in ['title', 'description']:
+                record['contains-sept'] = [phrases_in_str('wrzes', 'wrześ', target=str(record[place]))]
+                record['contains-oct'] = [phrases_in_str('pazd', 'paźd', target=str(record[place]))]
+                
+                # tu możesz dodawać różne filtry
+                
+            record = pd.DataFrame(record)
             self.offers_df = pd.concat([self.offers_df, record])
+            
         self.offers_df.reset_index(inplace=True, drop=True)
-    
-    def inspect_offer(self, idx):
-        page = requests.get(str(self.offers_df.loc[idx, 'link']))
-        soup = bs4.BeautifulSoup(page.content, 'html.parser')
-        details = soup.find('table', class_='details fixed marginbott20 margintop5 full')
-        details = [d.text for d in soup.find_all('th')]
-        det_values = [d.text.strip() for d in soup.find_all('td', class_='value')]
-        dets = zip(details, det_values)
-        text_content = soup.find('div', class_="clr lheight20 large", id="textContent")
-        
-        if text_content is None:
-            text_content = ''
-        else:
-            text_content = text_content.text
-
-        for det, value in dets:
-            self.offers_df.loc[idx, det] = value
-        # input(self.offers_df.iloc[idx])
-        # input(text_content)
-        # self.offers_df['text'] = ''
-        # input(self.offers_df.columns)
-        self.offers_df.at[idx, 'text'] = text_content
-    
-    def search_for_april(self, idx):
-        phrases = ['kwiet', '01.04', '01/04', '1/04', '01/4', '01-04', '01-4', '1-04', '1-4']
-        match = False
-
-        if any(phrase in self.offers_df.at[idx, 'text'] for phrase in phrases):
-            match = True
-        if any(phrase in self.offers_df.at[idx, 'title'] for phrase in phrases):
-            match = True
-
-        self.offers_df.at[idx, 'contains_april'] = match
-    
-    def analyze(self):
-        for idx, _ in self.offers_df.iterrows():
-            print(f'Analyzing {idx}')
-            self.inspect_offer(idx)
-            self.search_for_april(idx)
-
 
 
 h = HouseMetaData()
-# h.max_pages = 1
-for i in range(h.max_pages):
-    h.next_page()
-    h.source_page()
-    h.extract_all_offer_data()
-print('analyzing')
-h.analyze()
-h.offers_df.drop('text', 1, inplace=True)
-print('DONE')
-# print(h.offers_df.columns)
-# print(h.offers_df[['title', 'price', 'contains_april', 'text']])
+h.max_pages = 2
 
-h.offers_df.to_csv(os.path.join(os.getcwd(), 'homes.csv'))
+for i in range(h.max_pages):
+    print(f'Analyzing page {i}')
+    h.source_href(i)
+    h.extract_all_offer_data()
+
+print('DONE')
+
+h.offers_df[['price', 'contains-sept', 'contains-oct', 'title', 'description', 'link']].to_csv(os.path.join(os.getcwd(), 'homes.csv'))
 
